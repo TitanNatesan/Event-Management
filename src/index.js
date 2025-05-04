559ce3f8e37d7185ad41477a8e1d867bc3c5e34d
Titan Natesan <143887015+TitanNatesan@users.noreply.github.com>
2025-05-04 21:15:17 +0530

    its ok now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 console.log('Server is starting...');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json()); // Middleware to parse JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Middleware to parse JSON request bodies
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const { default: EventModel } = require('./models');
@@ -19,6 +23,7 @@ mongoose.connect(mongoURI).then(() => {
 });
 
 app.post("/events", async (req, res) => {
+    console.log("POST /events", req.body);
     const { title, description, date } = req.body;
     const newEvent = new EventModel({ title, description, date });
     await newEvent.save();
@@ -26,11 +31,13 @@ app.post("/events", async (req, res) => {
 });
 
 app.get("/events", async (req, res) => {
+    console.log("GET /events");
     const EventData = await EventModel.find();
     res.status(200).json(EventData);
 });
 
 app.get("/events/:id", async (req, res) => {
+    console.log("GET /events/:id", req.params.id);
     const { id } = req.params;
     const EventData = await EventModel.findById(id);
     if (!EventData) { return res.status(404).json({ message: "Event not found" }) }
@@ -38,6 +45,7 @@ app.get("/events/:id", async (req, res) => {
 });
 
 app.put("/events/:id", async (req, res) => {
+    console.log("PUT /events/:id", req.params.id, req.body);
     const { id } = req.params;
     const { title, description, date } = req.body;
     const EventData = await EventModel.findByIdAndUpdate(id, { title, description, date }, { new: true });
@@ -46,6 +54,7 @@ app.put("/events/:id", async (req, res) => {
 });
 
 app.delete("/events/:id", async (req, res) => {
+    console.log("DELETE /events/:id", req.params.id);
     const { id } = req.params;
     const EventData = await EventModel.findByIdAndDelete(id);
     if (!EventData) { return res.status(404).json({ message: "Event not found" }) }
